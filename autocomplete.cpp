6e24382c72951a758057213799a2f2af1e468c5a
warrengalyen <wgalyen@gmail.com>
2016-12-16 09:12:17 -0800

    Initial commit

---
+++ autocomplete.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include "autocomplete.h"
+
+using namespace std;
+
+void AutoComplete::addToWordset(vector<string> wordset)
+{
+    if (isCaseInsensitive) {
+        for (auto &str : wordset) {
+            std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+        }
+    }
+    trie->addToWordset(wordset);
+}
+
+void AutoComplete::addToWordset(string word)
+{
+    if (isCaseInsensitive) {
+        std::transform(word.begin(), word.end(), word.begin(), ::tolower);
+    }
+    trie->addToWordset(word);
+}
+
+bool AutoComplete::canResolve(string str) 
+{
+    return trie->canResolve(str);
+}
+
+string AutoComplete::resolveString(string str)
+{
+    return trie->resolveString(str);
+}
+
+vector<string> AutoComplete::getSuggestions(string str)
+{
+    return trie->getSuggestions(str);
+}
+
+AutoComplete::AutoComplete(std::vector<std::string> wordset, bool isCaseInsensitive) : isCaseInsensitive{isCaseInsensitive}
+{
+    if (isCaseInsensitive) {
+        for (auto &str : wordset) {
+            std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+        }
+    }
+    trie = make_unique<Trie>(wordset);
+}
+
+AutoComplete::AutoComplete(bool isCaseInsensitive) : isCaseInsensitive {isCaseInsensitive}
+{
+    trie = make_unique<Trie>(vector<string>(0));
+}
---
+++ autocomplete.h
@@ -0,0 +1,24 @@
+#include <string>
+#include <memory>
+#include <vector>
+#include "trie.h"
+
+#ifndef AUTOCOMPLETE_H
+#define AUTOCOMPLETE_H
+
+class AutoComplete
+{
+    std::unique_ptr<Trie> trie;
+    bool isCaseInsensitive;
+
+public:
+    void addToWordset(std::vector<std::string> wordset);
+    void addToWordset(std::string word);
+    bool canResolve(std::string str);
+    std::string resolveString(std::string str);
+    std::vector<std::string> getSuggestions(std::string str);
+    AutoComplete(std::vector<std::string> type, bool isCaseInsensitive = false);
+    AutoComplete(bool isCaseInsensitive = false);
+};
+
+#endif
